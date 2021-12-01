9dd2de58d714ca7d0ccbc5ca4f4f5bf00235ed77
Douglas Thomson <djt210@gmail.com>
2021-12-01 05:47:34 +0000

    2021/day01

---
+++ 2021/day01/day01.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	log.Printf("Starting... %s", start.String())
+
+	testInputPtr := flag.Bool("testInput", false, "a bool")
+	flag.Parse()
+
+	if *testInputPtr {
+		input = inputTest
+	}
+
+	fmt.Println(len(input))
+
+	increases := 0
+
+	for i := range input {
+		if i == 0 {
+			continue
+		}
+
+		if input[i-1] < input[i] {
+			increases++
+		}
+	}
+
+	fmt.Println(increases)
+
+	sumArray := []int{}
+	increasesPart2 := 0
+	for i := range input {
+		if i < 2 {
+			continue
+		}
+		sumArray = append(sumArray, input[i]+input[i-1]+input[i-2])
+	}
+
+	for i := range sumArray {
+		if i == 0 {
+			continue
+		}
+
+		if sumArray[i-1] < sumArray[i] {
+			increasesPart2++
+		}
+
+	}
+
+	fmt.Println(increasesPart2)
+
+	elapsed := time.Since(start)
+	log.Printf("Duration: %s", elapsed)
+}
---
+++ 2021/day01/go.mod
@@ -0,0 +1,3 @@
+module github.com/dtrts/adventofcode/2021/day01
+
+go 1.17
